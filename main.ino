25247e6e727677bb892cda8d38585e4f4a16325f
Matt Kaufman <98175105+makaufmanUI@users.noreply.github.com>
2022-07-31 01:18:14 -0500

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,19 +1,17 @@
-#include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
-#include <Arduino.h>
-#include <Adafruit_SHT31.h>
 #include <Wire.h>
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
 #include <Arduino_JSON.h>
-
-
-String apiKey = "8C5RJLITHO074GJ5";               // Thingspeak write API key
-const char *ssid = "Not Creepy Van Outside";      // WiFi name
-const char *pass = "Athlete99!";                  // WiFi password
-const char* server = "api.thingspeak.com";        // Thingspeak API server
-
-WiFiClient client;
-
+#include <Adafruit_SHT31.h>
+#include <ESP8266HTTPClient.h>
 Adafruit_SHT31 sht31 = Adafruit_SHT31();
+
+
+String apiKey = "";                               // Thingspeak write API key
+const char* server = "api.thingspeak.com";        // Thingspeak API server URL
+const char *ssid = "Not Creepy Van Outside";      // WiFi name
+const char *pass = "";                            // WiFi password
+WiFiClient client;
 
 
 void setup()
@@ -25,7 +23,7 @@ void setup()
   Serial.println(ssid);
   WiFi.begin(ssid, pass);
 
-  while ( WiFi.status() != WL_CONNECTED )
+  while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
     Serial.print(".");
@@ -35,7 +33,7 @@ void setup()
   Serial.println("WiFi connected!");
   Serial.println();
 
-  if ( ! sht31.begin(0x44) )                      // Set to 0x45 for alternate I2C address
+  if (!sht31.begin(0x44))
   {
     Serial.println("Couldn't connect to SHT31");
     while (1) delay(1);
@@ -51,7 +49,7 @@ void loop()
   float h = sht31.readHumidity();
   float tf = ( t * 1.8 ) + 32;
 
-  if ( client.connect(server, 80) )               // "184.106.153.149" or api.thingspeak.com
+  if (client.connect(server, 80))                 // "184.106.153.149" or api.thingspeak.com
   {
     delay(100);
     String sendData = apiKey+"&field1="+String(tf)+"&field3="+String(h)+"\r\n\r\n";
